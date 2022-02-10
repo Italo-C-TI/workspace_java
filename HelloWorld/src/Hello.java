0c692122c2a69d23f49f510c080ac6ea22d07d9a
Italo Costa <costa.italo.ti@gmail.com>
2022-02-10 09:17:52 -0300

    criação de feature de aprendizado

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,12 +1,4 @@
-import java.util.Scanner;
-
-public class Hello {
-	public static void main(String[] args) {
-		Scanner teclado = new Scanner(System.in);
+public class Hello {	public static void main(String[] args) {
 		System.out.println("hello world!!!!");
-
-		int valor = teclado.nextInt();
-		System.out.println("Voce digitou " + valor);
-		teclado.close();
 	}
 }
