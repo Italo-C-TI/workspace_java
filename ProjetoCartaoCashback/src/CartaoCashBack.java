f69fa1f79212e53e03b3caf69db95dbbbe3b54f4
Italo Costa <costa.italo.ti@gmail.com>
2022-02-15 13:20:25 -0300

    Exercicio de herança e polimorfismo

---
+++ ProjetoCartaoCashback/src/CartaoCashBack.java
@@ -0,0 +1,38 @@
+
+public class CartaoCashBack extends CartaoPrePago {
+	private int tipo; //bronze/silver/gold
+
+	public CartaoCashBack(String numeroCartao, String nomeTitular, int anoValidade, int mesValidade,int tipo) {
+		super(numeroCartao, nomeTitular, anoValidade, mesValidade);
+		this.tipo = tipo;
+	}
+	
+	public boolean comprar(double valor) {
+		if(super.comprar(valor) && tipo>=0 && tipo<=2) {
+			if (tipo==0) {
+				this.saldo+=valor*0.02;				
+			}else if(tipo == 1) {
+				this.saldo+=valor*0.05;		
+			}else if(tipo == 2) {
+				this.saldo+=valor*0.08;	
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " tipo = "+ (tipo == 2?"gold":tipo == 1?"silver":"bronze");
+	}
+	
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+}
---
+++ ProjetoCartaoCashback/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		CartaoPrePago c1;
+		CartaoPrePago c2;
+		
+		c1 = new CartaoPrePago("98765430","Italo",2030,8);
+		c2 = new CartaoCashBack("000000010","Italo2",2031, 5, 1);
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		
+		c1.adicionarCredito(150.50);
+		c2.adicionarCredito(150.50);
+		c1.comprar(100);
+		c2.comprar(100);
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		input.close();
+	}
+}
